d83804f7735a1c4f25d5ea5b7f49f1b812dbf308
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2024-01-10 10:59:06 +0800

    added main.js

---
+++ script/main.js
@@ -0,0 +1,12 @@
+// glightbox
+const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true
+});
+
+var lightboxDescription = GLightbox({
+    selector: '.glightbox1'
+});
+
+// isotope js sorting
